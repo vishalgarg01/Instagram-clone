6f1ab1f4e9643d44fff3ce6f19ebc5a8996abdf8
vishalgarg01 <vishalgarghi@gmail.com>
2021-02-19 15:50:31 +0530

    adding comments

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -63,6 +63,30 @@ const Home=()=>{
         }).catch(err=>{console.log(err)})
     }
 
+    const makeComment=(text,postId)=>{     
+        fetch('/comment',{
+            method:'put',
+            headers:{
+                "Content-Type":"application/json",
+                "authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId,
+                text
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            // console.log(result)
+            const newData=data.map(item=>{
+                if(item._id==result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err=>{console.log(err)})
+    }
     return(
         <div className="home">
             {
@@ -74,21 +98,33 @@ const Home=()=>{
                                 <img src={item.photo} alt=""/> 
                             </div>
                             <div className="card-content">
-                            <i className="material-icons" style={{color:'red'}}>favorite</i>
-                            {
-                                //show user like or unlike button on bases of weather he already liked or not
-                            item.likes.includes(state.user._id)
-                            ?
-                            <i className="material-icons"
-                            onClick={()=>{unlikePost(item._id)}}>thumb_down</i>
-                            :
-                            <i className="material-icons"
-                            onClick={()=>{likePost(item._id)}}>thumb_up</i>
-                            }
+                                <i className="material-icons" style={{color:'red'}}>favorite</i>
+                                {
+                                    //show user like or unlike button on bases of weather he already liked or not
+                                item.likes.includes(state.user._id)
+                                ?
+                                <i className="material-icons"
+                                onClick={()=>{unlikePost(item._id)}}>thumb_down</i>
+                                :
+                                <i className="material-icons"
+                                onClick={()=>{likePost(item._id)}}>thumb_up</i>
+                                }
                                 <h6>{item.likes.length} likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
-                                <input type="text" placeholder="add comment"/>
+                                {
+                                    item.comments.map(record=>{
+                                        return(
+                                            <h6 key={record._id}><span style={{fontWeight:"500"}}>{record.postedBy.name} </span>{record.text}</h6>
+                                        )
+                                    })
+                                }
+                                <form onSubmit={(e)=>{
+                                    e.preventDefault()
+                                    makeComment(e.target[0].value,item._id)
+                                }}>
+                                    <input type="text" placeholder="add comment"/>
+                                </form>
                             </div>
                       </div>
                     );
