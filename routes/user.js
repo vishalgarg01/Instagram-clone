19e925b871f863b44ed450d2ba4b685348fb23b8
vishalgarg01 <vishalgarghi@gmail.com>
2021-02-25 12:32:05 +0530

    Updating profile pic API

--- routes/user.js
+++ routes/user.js
@@ -68,5 +68,14 @@ router.put('/unfollow',requirelogin,(req,res)=>{
     }
     )
 })
+router.put('/updatepic',requirelogin,(req,res)=>{
+    User.findByIdAndUpdate(req.user._id,{$set:{pic:req.body.pic}},{new:true},
+        (err,result)=>{
+         if(err){
+             return res.status(422).json({error:"pic canot post"})
+         }
+         res.json(result)
+    })
+})
 
 module.exports=router;
